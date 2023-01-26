0e5a338389b3cdea00386d2e1458d42276355a53
Ilya Dorofeev <dara321@yandex.ru>
2023-01-26 22:11:15 +0300

    3zada4i

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. С клавиатуры вводится целое число. Вывести квадрат числа 
+
+string? S;
+int A;
+S=Console.ReadLine ();
+
+Console.WriteLine(S);
+A=Convert.ToInt32(S);
+Console.WriteLine(Math.Pow (A,2));
--- 002/Program.cs
+++ 002/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 2. С клавиатуры вводятся два вещественных числа. Проверять является ли одно из них квадратом второго
+double a,b;
+string s;
+s=Console.ReadLine();
+a=Convert.ToDouble(s);
+s=Console.ReadLine();
+b=Convert.ToDouble(s);
+if (a*a==b)
+System.Console.WriteLine("b является квадратом а");
+if (b*b==a)
+System.Console.WriteLine("a является квадратом b");
