3779beccffd45f1bb37a30bfc10284f474c0f994
Ilya Dorofeev <dara321@yandex.ru>
2023-01-28 14:25:18 +0300

    VseZada4i

--- 009/Program.cs
+++ 009/Program.cs
@@ -1,10 +1,8 @@
 ﻿// 9. Вывести на экран четные числа от 1 до N
 int i=2;
-int N=10;
+int N;
+Console.Write("Введите N: ");
     N=Convert.ToInt32(Console.ReadLine());
-//нц пока i<=N
-//кц
-
 while(i<=N)
 {
     System.Console.WriteLine(i);
